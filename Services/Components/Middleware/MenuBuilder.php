7ec94b8ea5f55722b3ad234771d30cb4c24178d6
Didier Fanchon <didier.fanchon@webtales.fr>
2018-12-04 17:02:20 +0100

    Update Services/Components/Middleware/MenuBuilder.php

--- Services/Components/Middleware/MenuBuilder.php
+++ Services/Components/Middleware/MenuBuilder.php
@@ -29,7 +29,7 @@ class MenuBuilder
             $page["active"] = $component['context']['page']['id'] == $page['id'];
             foreach ($page["children"] as &$subpage) {
                 $destinationPage = $this->getDestinationPage($subpage, $component['itemConfig']['settings']);
-                $subpage["url"] = $routeBuilder->getRoute($component["context"]["site"]["id"],$subpage["id"],$isSecure,$component["context"]['previewMode'],$component["context"]['baseUrl'],$subpage["urlSegment"]);
+                $subpage["url"] = $routeBuilder->getRoute($component["context"]["site"]["id"],$destinationPage,$isSecure,$component["context"]['previewMode'],$component["context"]['baseUrl'],$subpage["urlSegment"]);
             }
             $component["pageTree"]["children"][] = $page;
         }
