0a8894e0cbdf8bed7eb1f1502d8ae6dd975db73b
Didier Fanchon <didier.fanchon@webtales.fr>
2018-12-19 13:20:28 +0100

    Update Services/Components/Middleware/MenuBuilder.php

--- Services/Components/Middleware/MenuBuilder.php
+++ Services/Components/Middleware/MenuBuilder.php
@@ -20,11 +20,11 @@ class MenuBuilder
         $menuData = app()['DataGraphQLHandler']->execute('query q{wp_pages(parentId:0){name id type urlSegment itemId children(limit:1000,orderBy:"order",orderByDirection:"ASC"){name order id parentId itemId type urlSegment}}}',null,null);
         foreach ($menuData["data"]["wp_pages"] as $page){
             $destinationPage = $this->getDestinationPage($page, $component['itemConfig']['settings']);
-            $page["url"]=$routeBuilder->getRoute($component["context"]["site"]["id"],$destinationPage,$isSecure,$component["context"]['previewMode'],"/",$page["urlSegment"],$page["itemId"]);
+            $page["url"]=$routeBuilder->getRoute($component["context"]["site"]["id"],$destinationPage,$isSecure,$component["context"]['previewMode'],$component["context"]['baseHost'],$page["urlSegment"],$page["itemId"]);
             $page["active"] = $component['context']['page']['id'] == $page['id'];
             foreach ($page["children"] as &$subpage) {
                 $destinationPage = $this->getDestinationPage($subpage, $component['itemConfig']['settings']);
-                $subpage["url"] = $routeBuilder->getRoute($component["context"]["site"]["id"],$destinationPage,$isSecure,$component["context"]['previewMode'],"/",$subpage["urlSegment"],$subpage["itemId"]);
+                $subpage["url"] = $routeBuilder->getRoute($component["context"]["site"]["id"],$destinationPage,$isSecure,$component["context"]['previewMode'],$component["context"]['baseHost'],$subpage["urlSegment"],$subpage["itemId"]);
             }
             $component["pageTree"]["children"][] = $page;
         }
