13dbaab8a329e54ef16fc2608e06d4a7a6efd559
Didier Fanchon <didier.fanchon@webtales.fr>
2019-03-21 08:59:05 +0100

    Change read cache middleware

--- bootstrap.php
+++ bootstrap.php
@@ -71,7 +71,7 @@ if(!empty($_SERVER["HTTP_HOST"])&&in_array($_SERVER["HTTP_HOST"],$authorizedDoma
             'routeResolver' => App\Extensions\WordpressConnector\Middleware\WPRouteResolver::class
         ]);
         if (config('hasFrontCache')) {
-            $middlewares = ['globalCache', 'publicNS', 'configLoader', 'preResolver', 'routeResolver'];
+            $middlewares = ['readFromCache', 'publicNS', 'configLoader', 'preResolver', 'routeResolver'];
         } else { 
             $middlewares = ['publicNS', 'configLoader', 'preResolver', 'routeResolver'];
         }
